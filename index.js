ed0b2434c2fd6f789f52fb1a03fe760bf9555ae6
Sebastian Thomas <sebastian_34260@web.de>
2022-02-07 20:54:03 +0100

    Fix available bug

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.post('/names', async (req, res) => {
     console.log(names.data);
     res.status(200).json({
       names: names.data.names,
-      available: names.data.available,
+      available: names.data.a,
     });
   } catch (err) {
     console.log('Error' + err.response.data.msg);
